e778cefe300eef845f5a71ebd7bfa4e40fda5a70
endersonpb <endersonpb0@gmail.com>
2023-04-24 20:46:38 -0300

    a

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from api.serializers import FundoImobiliarioSerializer
+from rest_framework import viewsets, permissions
+from api.models import FundoImobiliario
+
+
+class FundoImobiliarioViewSet(viewsets.ModelViewSet):
+  queryset = FundoImobiliario.objects.all()
+  serializer_class = FundoImobiliarioSerializer
+  permission_classes = [permissions.IsAuthenticated]
